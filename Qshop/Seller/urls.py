302eb606c02d957747ef80df6f3e5343f2ea5844
zhangsan <zhangsan@qq.com>
2019-10-07 23:09:37 +0800

    项目centos未部署

--- Qshop/Seller/urls.py
+++ Qshop/Seller/urls.py
@@ -5,8 +5,8 @@ from Seller.views import *
 urlpatterns = [
     # path('register/', cache_page(60*15)(register)),
     path('register/',(register)),
-    path('login/', login),
     path('index/', index),
+    path('login/', login),
     path('logout/', logout),
     path('goods_list/', goods_list),
     re_path('goods_list/(?P<page>\d+)/(?P<status>[01])/', goods_list),
